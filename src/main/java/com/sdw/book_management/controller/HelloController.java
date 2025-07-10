7f02d406577e1920f2cae721da29a75fa9d607fc
sdw98 <sdw9851@gmail.com>
2025-07-10 15:27:40 +0900

    first commit

---
+++ src/main/java/com/sdw/book_management/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.sdw.book_management.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+    @GetMapping
+    public String sayHello() {
+        return "hello";
+    }
+
+    @PostMapping
+    public String sayPostHello() {
+        return "post hello";
+    }
+
+    @PutMapping
+    public String sayPutHello() {
+        return "put hello";
+    }
+
+    @DeleteMapping
+    public String sayDeleteHello() {
+        return "delete hello";
+    }
+}
---
+++ src/test/java/com/sdw/book_management/BookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdw.book_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
